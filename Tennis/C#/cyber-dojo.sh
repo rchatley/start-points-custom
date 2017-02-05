5be804fc3646d7c1172e352ac0ad72fc39ea4120
Jon Jagger <jon@jaggersoft.com>
2017-02-05 13:21:59 +0000

    updated C# cyber-dojo.sh files for new runner

--- Tennis/C#/cyber-dojo.sh
+++ Tennis/C#/cyber-dojo.sh
@@ -1,4 +1,11 @@
-dmcs -t:library -r:/usr/lib/cli/nunit.framework-2.6/nunit.framework.dll -out:RunTests.dll *.cs
+NUNIT_PATH=/nunit/NUnit.2.6.2/lib
+export MONO_PATH=${NUNIT_PATH}
+
+mcs -t:library \
+  -r:${NUNIT_PATH}/nunit.framework.dll \
+  -out:RunTests.dll *.cs
+
 if [ $? -eq 0 ]; then
-  nunit-console -nologo RunTests.dll
+  NUNIT_RUNNERS_PATH=/nunit/NUnit.Runners.2.6.1/tools
+  mono ${NUNIT_RUNNERS_PATH}/nunit-console.exe -nologo ./RunTests.dll
 fi
